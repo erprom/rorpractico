10a864fce8d04bab6e5738bc07b18b539d337542
erprom <erprom@gmail.com>
2018-04-04 17:39:45 +0200

    formulario ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
-
+	def show
+		@user = User.find(params[:id])
+	end
 	def new
+	end
+	def create
+		@user = User.new(user_params)
+		@user.save
+		redirect_to @user
+	end
+	private 
+	def user_params
+		params.require(:user).permit(:nombre, :apellido, :email)
 	end
 end
