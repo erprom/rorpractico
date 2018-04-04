5cc60a8acab705fa9b85c321abefacd7b8648e30
erprom <erprom@gmail.com>
2018-04-04 18:38:58 +0200

    validacion form usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :nombre,:apellido, presence: true,length:{minimum: 3}
+	validates :email, presence:true,format:{ with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+
 end
