cabbda067e6202fbbf0d9260b192f96d0e8a76e3
erprom <erprom@gmail.com>
2018-04-04 19:00:40 +0200

    plantilla form y opcion editar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,26 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
 			redirect_to @user
 		else
 			render 'new'
+		end
 	end
-end
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+    		redirect_to @user
+  		else
+    		render 'edit'
+  		end
+  	end
 	private 
 	def user_params
 		params.require(:user).permit(:nombre, :apellido, :email)
