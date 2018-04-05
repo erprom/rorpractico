aad1506fce665cb8bbade2a600a03a266566f044
erprom <erprom@gmail.com>
2018-04-05 15:46:01 +0200

    configurado form animales para new y edit

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,15 +1,37 @@
 class AnimalsController < ApplicationController
+	def index
+		@user = User.find(params[:user_id])
+		@animal = @user.animals.find(params[:id])
+	end
 	def show
 		@user = User.find(params[:user_id])
 	    @animal = @user.animals.find(params[:id])
 	end
 	def new
 		@user = User.find(params[:user_id])
+		@animal = @user.animals.new
+	end
+	def edit
+		@user = User.find(params[:user_id])
+	    @animal = @user.animals.find(params[:id])
 	end
 	def create
 	    @user = User.find(params[:user_id])
 	    @animal = @user.animals.create(animal_params)
+	    if @animal.save
 	    redirect_to user_path(@user)
+		else
+			render 'new'
+		end
+  	end
+  	def update
+  		@user = User.find(params[:user_id])
+  		@animal = @user.animals.find(params[:id])
+  		if @animal.update(animal_params)
+  			redirect_to user_path(@user)
+  		else
+  			render 'edit'
+  		end
   	end
   	def destroy
 	    @user = User.find(params[:user_id])
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,4 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  validates :nombre,length:{minimum: 3, message:"introduce mas de 3 caracteres"}
 end
