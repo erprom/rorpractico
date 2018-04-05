be403c2583abad80eddf1f24c992099808fe5bf2
erprom <erprom@gmail.com>
2018-04-05 09:40:29 +0200

    configurando modelo animals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-		validates :nombre,:apellido,length:{minimum: 3, message:"introduce mas de 3 caracteres"}
-	validates :email,format:{ with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message:"correo incorrecto"}
+	has_many :animals
+	validates :nombre,:apellido,length:{minimum: 3, message:"introduce mas de 3 caracteres"}
+	validates :email,format:{ with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+	message:"correo incorrecto"}
 
 end
