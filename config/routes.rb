be7304b33b8f1737c8a0fa3d9f1f90ba3fd1fe60
erprom <erprom@gmail.com>
2018-04-05 10:46:55 +0200

    animals ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'principal/index'
   root 'principal#index'
-  resources :users
+  resources :users do
+  	resources :animals
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
