0eb13ac615082eb6bb58159330dc9c94d87a70ca
erprom <erprom@gmail.com>
2018-04-05 23:31:57 +0200

    añadido error en animals

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -17,14 +17,17 @@ class AnimalsController < ApplicationController
 	end
 	def create
 	    @user = User.find(params[:user_id])
+	    #Establecemos limite de animales para user
 	    if @user.animals.count <6
 	    	@animal = @user.animals.create(animal_params)
 		    if @animal.save
 		    	redirect_to user_path(@user)
 			else
+				@errors="falta algun dato por rellenar"
 				render 'new'
 			end
 			else
+				@errors="Tienes mas de 5 animales"
 			render 'new'
 		end
   	end
@@ -34,6 +37,7 @@ class AnimalsController < ApplicationController
   		if @animal.update(animal_params)
   			redirect_to user_path(@user)
   		else
+  			@errors="falta algun dato por rellenar"
   			render 'edit'
   		end
   	end
